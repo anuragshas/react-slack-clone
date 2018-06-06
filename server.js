c777c2642007a0a1190b0795831c2bd756f2f78d
anurag <anurag.comm@gmail.com>
2018-06-06 14:51:12 +0530

    added pusher

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:2c28e8eb-bba2-4821-b734-f4d262c64726',
+  key: '5001d5ee-5f4b-47e3-86df-0748acc6f08e:+SI6m5vwjHQfTbXwMeM2+swCFDfm0Pb0MwlKk5qZvFA=',
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error_type === 'services/chatkit/user_already_exists') {
+        res.sendStatus(200)
+      } else {
+        res.status(error.status).json(error)
+      }
+    })
+})
 
 const PORT = 3001
 app.listen(PORT, err => {
